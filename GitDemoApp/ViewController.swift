f1cfdb22851860e8116566fb1ab30fd1eaf73de8
abrar  ul haq <abrarulhaq.ios@gmail.com>
2017-01-24 15:15:38 +0500

    Initial Commit

---
+++ GitDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoApp
+//
+//  Created by abrar  ul haq on 24/01/2017.
+//  Copyright © 2017 locopixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
